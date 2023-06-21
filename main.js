5dba5c15a77d7b163ccc9ac412b6c403092ebbfa
ayichung <121764547+ayichung@users.noreply.github.com>
2023-06-21 14:55:00 -0400

    Implement grid cell fill on hover

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
-/* global vars */
+/* grid */
 const gridCont = document.querySelector(".grid-container");
 dim = 16;
 
-/* 16x16 grid */
-gridCont.setAttribute("id", dim.toString());
-for (let i=0; i<dim; i++) {
-    const gridRow = document.createElement("div");
-    gridRow.setAttribute("class", "grid-row");
-    for (let j=0; j<dim; j++) {
-        const gridCell = document.createElement("div");
-        gridCell.setAttribute("class", "grid-cell");
-        gridRow.appendChild(gridCell);
+function createGrid(dim) {
+    gridCont.setAttribute("id", dim.toString());
+    for (let i=0; i<dim; i++) {
+        const gridRow = document.createElement("div");
+        gridRow.setAttribute("class", "grid-row");
+        for (let j=0; j<dim; j++) {
+            const gridCell = document.createElement("div");
+            gridCell.setAttribute("class", "grid-cell");
+            gridRow.appendChild(gridCell);
+        }
+        gridCont.appendChild(gridRow);
     }
-    gridCont.appendChild(gridRow);
+}
+
+createGrid(dim);
+
+/* color */
+const gridCells = document.querySelectorAll(".grid-cell");
+gridCells.forEach(gridCell => gridCell.addEventListener("mouseover", () => changeColor(gridCell)));
+
+function changeColor (gridCell) {
+    gridCell.classList.add("fill");
 }
